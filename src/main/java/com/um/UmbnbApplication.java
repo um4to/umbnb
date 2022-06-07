2f5583bbb6391fbdda50734f6688bb5f98490378
DelfinaQuinteros <65193862+DelfinaQuinteros@users.noreply.github.com>
2022-06-07 20:25:05 -0300

    umbnb

---
+++ src/main/java/com/um/UmbnbApplication.java
@@ -0,0 +1,12 @@
+package com.um;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmbnbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UmbnbApplication.class, args);
+	}
+}

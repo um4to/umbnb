eb90784d71d017a7e53cd050967328c0af55dcdf
DelfinaQuinteros <65193862+DelfinaQuinteros@users.noreply.github.com>
2022-06-07 20:48:55 -0300

    umbnb

--- src/main/java/com/um/UmbnbApplication.java
+++ src/main/java/com/um/UmbnbApplication.java
@@ -1,13 +1,12 @@
-package com.example.demo;
+package com.um;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class UmbnbApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(UmbnbApplication.class, args);
 	}
-
 }
--- src/test/java/com/um/umbnb/UmbnbApplicationTests.java
+++ src/test/java/com/um/umbnb/UmbnbApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.um.umbnb;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class UmbnbApplicationTests {
 
 	@Test
 	void contextLoads() {
